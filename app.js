0024e6f8f90915843b4ba15ab03689ef42ac1c70
Nihal Chehade <nihalche@icloud.com>
2024-01-15 01:04:47 -0800

    final commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const toggleSwitch = document.querySelector(`input[type="checkbox"]`);
+if (localStorage.getItem("darkModeEnabled")) {
+  document.body.className = "dark";
+  toggleSwitch.checked = true;
+}
+
+toggleSwitch.addEventListener("click", function () {
+  const  checked  = toggleSwitch.checked;
+  if (checked) {
+    localStorage.setItem("darkModeEnabled", true);
+  } else {
+    localStorage.removeItem("darkModeEnabled");
+  }
+  document.body.className = checked ? "dark" : "";
+});
